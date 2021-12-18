b135e2dd5bf89cc14211460000d5dd743cd3dba0
OnDemand <40256173+billrain@users.noreply.github.com>
2021-12-18 14:01:09 +0800

    1

---
+++ server.py
@@ -0,0 +1,22 @@
+import tornado.ioloop
+import tornado.web
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        title = "Hello This is OneBlockchain.sg"
+        bgcolor = "darkred"
+        self.render("template.html", title=title, bgcolor=bgcolor)
+        print(self.request)
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
